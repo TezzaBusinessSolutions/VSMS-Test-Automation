e19f7f703688da34c939ef6627785cfb08dfed84
Gituto <dennisgituto1@gmail.com>
2017-10-06 12:16:29 +0300

    Add new Visitor in Security Management Module

---
+++ Test Suites/Add Visitor.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Add Visitor in Security Management System</description>
+   <name>Add Visitor</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-10-06T12:10:10</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e127f2d4-fb5b-46a1-a811-bbee22cb2b8c</testSuiteGuid>
+   <testCaseLink>
+      <guid>95263196-4430-4632-8eb1-09dd8d58e008</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e92bb936-e82b-4503-8603-4b7bd65b249c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add new Visitor in Security Management Module</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

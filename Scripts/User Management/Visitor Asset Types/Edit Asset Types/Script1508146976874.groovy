25fe2e0800da960fc07910d5d1517a86d1f5dcd8
stlireri <stellaireri@gmail.com>
2017-10-16 13:21:10 +0300

    Editing Asset types

---
+++ Scripts/User Management/Visitor Asset Types/Edit Asset Types/Script1508146976874.groovy
@@ -0,0 +1,41 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('LOGIN'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('User Management/Visitor Types/a_Modules'))
+
+WebUI.click(findTestObject('User Management/Visitor Types/a_User  Management'))
+
+WebUI.click(findTestObject('User Management/Visitor Types/a_User Settings'))
+
+WebUI.click(findTestObject('User Management/Visitor Types/a_Manage Visitor Types'))
+
+WebUI.click(findTestObject('User Management/Visitor Asset Types/i_fa fa-edit'))
+
+WebUI.setText(findTestObject('User Management/Visitor Asset Types/edit_asset_description'), var_editAssetName)
+
+WebUI.click(findTestObject('User Management/Visitor Asset Types/button_Update'))
+
+WebUI.delay(2)
+
+WebUI.closeBrowser()
+
